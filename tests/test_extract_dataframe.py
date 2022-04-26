21ab363f5c6f62977bf4f47587d63e297400c6f1
Michael Tamirie <michaeltamirie@gmail.com>
2022-04-26 23:54:30 +0300

    Fix the path issues

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -1,13 +1,12 @@
-from extract_dataframe import TweetDfExtractor
-from extract_dataframe import read_json
 import unittest
 import pandas as pd
 import sys
 import os
-from textblob import TextBlob
 
-sys.path.append(os.path.abspath(os.path.join('../..')))
+sys.path.append(os.path.abspath(os.path.join('..')))
 
+from extract_dataframe import TweetDfExtractor
+from extract_dataframe import read_json
 
 _, tweet_list = read_json("./data/Economic_Twitter_Data.json")
 
