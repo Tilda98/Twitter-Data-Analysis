6a374ca9eee4de19a0a8a9f17e95e37ca7431168
Tildaa98 <awuormatilda85@gmail.com>
2022-04-27 18:36:27 +0300

    Added missing code

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -62,51 +62,80 @@ class TweetDfExtractor:
 
         return polarity, subjectivity
 
-    def find_created_time(self)->list:
-       
-        return created_at
-
-    def find_source(self)->list:
-        source = 
+    def find_source(self) -> list:
+        source = [tweet['source'] for tweet in self.tweets_list]
 
         return source
 
-    def find_screen_name(self)->list:
-        screen_name = 
+    def find_screen_name(self) -> list:
+        screen_name = [tweet['user']['screen_name']
+                       if 'user' in tweet else '' for tweet in self.tweets_list]
 
-    def find_followers_count(self)->list:
-        followers_count = 
+        return screen_name
 
-    def find_friends_count(self)->list:
-        friends_count = 
+    def find_followers_count(self) -> list:
+        followers_count = [tweet['user']['followers_count']
+                           if 'user' in tweet else '' for tweet in self.tweets_list]
 
-    def is_sensitive(self)->list:
+        return followers_count
+
+    def find_friends_count(self) -> list:
+        friends_count = [tweet['user']['friends_count']
+                         if 'user' in tweet else '' for tweet in self.tweets_list]
+
+        return friends_count
+
+    def is_sensitive(self) -> list:
         try:
-            is_sensitive = [x['possibly_sensitive'] for x in self.tweets_list]
+            is_sensitive = [tweet['possibly_sensitive']
+                            if 'possibly_sensitive' in tweet else None for tweet in self.tweets_list]
         except KeyError:
-            is_sensitive = None
+            is_sensitive = ""
 
         return is_sensitive
 
-    def find_favourite_count(self)->list:
-        
-    
-    def find_retweet_count(self)->list:
-        retweet_count = 
-
-    def find_hashtags(self)->list:
-        hashtags =
-
-    def find_mentions(self)->list:
-        mentions = 
-
-
-    def find_location(self)->list:
+    def find_favourite_count(self) -> list:
         try:
-            location = self.tweets_list['user']['location']
+            favorites_count = [tweet['retweeted_status']['favorite_count']
+                               if 'retweeted_status' in tweet else '' for tweet in self.tweets_list]
+
+        except KeyError:
+            favorites_count = ""
+
+        return favorites_count
+
+    def find_retweet_count(self) -> list:
+        try:
+            retweet_count = [tweet['retweeted_status']['retweet_count']
+                             if 'retweeted_status' in tweet else '' for tweet in self.tweets_list]
+        except KeyError:
+            retweet_count = ""
+        return retweet_count
+
+    def find_hashtags(self) -> list:
+        try:
+            hashtags = [tweet['retweeted_status']['entities']['hashtags']
+                        if 'retweeted_status' in tweet else '' for tweet in self.tweets_list]
+        except KeyError:
+            hashtags = ''
+        return 
+
+    def find_mentions(self) -> list:
+        try:
+            mentions = [tweet['retweeted_staus']['entities']['user_mentions']
+                        if 'extended_tweet' in tweet else "" for tweet in self.tweets_list]
+        except KeyError:
+            mentions = ""
+
+        return mentions
+
+    def find_location(self) -> list:
+        try:
+            location = [tweet['user']['location']
+                        if 'user' in tweet else "" for tweet in self.tweets_list]
         except TypeError:
             location = ''
-        
+
         return location
 
     
